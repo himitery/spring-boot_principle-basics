f59838f67a60f25b9e9093206386d915590a09fe
himitery <himit0131@gmail.com>
2021-12-23 00:03:59 +0900

    스프링 핵심 원리 이해1(예제 만들기): 프로젝트 생성

---
+++ src/test/java/study/springboot/principlebascis/PrincipleBascisApplicationTests.java
@@ -0,0 +1,13 @@
+package study.springboot.principlebascis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrincipleBascisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
