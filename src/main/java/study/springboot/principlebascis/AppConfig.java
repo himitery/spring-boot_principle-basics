4c268ba55c4bcb1dff08889c90eeedce037e946d
himitery <himit0131@gmail.com>
2021-12-24 01:56:20 +0900

    스프링 핵심 원리 이해2(객체 지향 원리 적용): AppConfig 리팩터링

--- src/main/java/study/springboot/principlebascis/AppConfig.java
+++ src/main/java/study/springboot/principlebascis/AppConfig.java
@@ -1,6 +1,8 @@
 package study.springboot.principlebascis;
 
+import study.springboot.principlebascis.discount.DiscountPolicy;
 import study.springboot.principlebascis.discount.FixDiscountPolicy;
+import study.springboot.principlebascis.member.MemberRepository;
 import study.springboot.principlebascis.member.MemberService;
 import study.springboot.principlebascis.member.MemberServiceImpl;
 import study.springboot.principlebascis.member.MemoryMemberRepository;
@@ -10,10 +12,18 @@ import study.springboot.principlebascis.order.OrderServiceImpl;
 public class AppConfig {
 
 	public MemberService memberService() {
-		return new MemberServiceImpl(new MemoryMemberRepository());
+		return new MemberServiceImpl(memberRepository());
+	}
+
+	private MemberRepository memberRepository() {
+		return new MemoryMemberRepository();
 	}
 
 	public OrderService orderService() {
-		return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+		return new OrderServiceImpl(memberRepository(), discountPolicy());
+	}
+
+	private DiscountPolicy discountPolicy() {
+		return new FixDiscountPolicy();
 	}
 }
