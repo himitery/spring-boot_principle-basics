a9c8bd21e4bb8723157c9a93a519245219d6fc05
himitery <himit0131@gmail.com>
2021-12-29 01:15:32 +0900

    의존관계 자동 주입: 조회 빈이 2개 이상 - 문제

--- src/main/java/study/springboot/principlebasics/discount/FixDiscountPolicy.java
+++ src/main/java/study/springboot/principlebasics/discount/FixDiscountPolicy.java
@@ -1,8 +1,10 @@
 package study.springboot.principlebasics.discount;
 
+import org.springframework.stereotype.Component;
 import study.springboot.principlebasics.member.Grade;
 import study.springboot.principlebasics.member.Member;
 
+@Component
 public class FixDiscountPolicy implements DiscountPolicy {
 
 	private final int discountFixAmount = 1000;
